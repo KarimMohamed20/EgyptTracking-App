d489784e809e6bfef905bf641c18e08de65ce9c2
Karim Mohamed <37942217+KarimMohamed20@users.noreply.github.com>
2020-10-27 08:00:49 +0200

    Change subscriptionId with subscriptionEmail

--- lib/Controller/OneSignal/notifications.dart
+++ lib/Controller/OneSignal/notifications.dart
@@ -1,19 +1,9 @@
-import 'dart:io';
-
 import 'package:app/Controller/API/config.dart';
-import 'package:device_info/device_info.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 class OneSignalNotifications {
   connect(String email) async {
-    if (Platform.isIOS == true) {
-      var deviceId = await DeviceInfoPlugin().iosInfo;
-      await OneSignal.shared.init(EnvData.oneSignalAppId);
-      await OneSignal.shared.setExternalUserId(deviceId.identifierForVendor);
-    } else if (Platform.isAndroid == true) {
-      var deviceId = await DeviceInfoPlugin().androidInfo;
-      await OneSignal.shared.init(EnvData.oneSignalAppId);
-      await OneSignal.shared.setExternalUserId(deviceId.androidId);
-    }
+    await OneSignal.shared.init(EnvData.oneSignalAppId);
+    await OneSignal.shared.setEmail(email: email);
   }
 }
