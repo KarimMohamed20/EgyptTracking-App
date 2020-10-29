89fc56ffdf10272e2bf5ea00b7ad9ec59dd116e0
Karim Mohamed <37942217+KarimMohamed20@users.noreply.github.com>
2020-10-29 10:29:57 +0200

    Update arrive.dart

--- lib/Controller/Driver/Ride/arrive.dart
+++ lib/Controller/Driver/Ride/arrive.dart
@@ -1,6 +1,18 @@
-class ArriveController {
-  Future arrive() async {
+import 'package:app/Controller/API/apiServices.dart';
+import 'package:app/Controller/API/config.dart';
+import 'package:app/Controller/Driver/Ride/get.dart';
+import 'package:app/Models/Ride/ride.dart';
+import 'package:get/get.dart';
 
-    
+class ArriveController {
+  DriverGetRides _rideModel = Get.find();
+  Future arrive(String studentId) async {
+    var res = await APIServices().put(
+      APIConfig.arriveStudent +
+          '?rideId=${_rideModel.currentRide.value.id}&studentId=$studentId',
+      body: {},
+    );
+    print(res.body);
+    return res;
   }
 }
